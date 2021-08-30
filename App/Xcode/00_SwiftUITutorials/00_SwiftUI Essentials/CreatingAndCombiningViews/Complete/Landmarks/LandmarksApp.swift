5979517e9d8609703d52c4053420379ef548142e
KANG SEUNG HOON <deforeturn@gmail.com>
2021-08-30 13:17:52 +0900

    SwiftUI Essential Tutorial 하나 함.

---
+++ App/Xcode/00_SwiftUITutorials/00_SwiftUI Essentials/CreatingAndCombiningViews/Complete/Landmarks/LandmarksApp.swift
@@ -0,0 +1,18 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+The top-level definition of the Landmarks app.
+*/
+
+import SwiftUI
+
+// @main은 앱의 진입 점을 식별함.
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

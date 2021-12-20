d812f90568e52f6e1bf71ea90ef597ff0b38fef5
Deforeturn <deforeturn@gmail.com>
2021-12-20 17:12:35 +0900

    SwiftTutorial: Layed RandMark app before begning.

---
+++ App/AppleDev/SwiftUITutorials/LandMark/LandMark/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LandMark
+//
+//  Created by Deforeturn on 12/20/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ App/AppleDev/SwiftUITutorials/LandMark/LandMark/LandMarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarkApp.swift
+//  LandMark
+//
+//  Created by Deforeturn on 12/20/21.
+//
+
+import SwiftUI
+
+@main
+struct LandMarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

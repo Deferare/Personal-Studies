3c8b674de181acb9d2886c29423d67f83dbf8b2c
Deforeturn <deforeturn@gmail.com>
2021-12-31 16:05:01 +0900

    SwiftUITutorial - Ongoing Drawing and Animation
    Doing Drawing Paths and Shape.

--- App/AppleDev/SwiftUITutorials/LandMark/LandMark/Views/LandMarks/LandmarkDetail.swift
+++ App/AppleDev/SwiftUITutorials/LandMark/LandMark/Views/LandMarks/LandmarkDetail.swift
@@ -8,7 +8,13 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
-    var landmark: Landmark
+    
+    @EnvironmentObject var modelData:ModelData
+    var landmark:Landmark
+    var landmarkIndex:Int{
+        modelData.landmarks.firstIndex(where: {$0.id == landmark.id})!
+    }
+    
     var body: some View {
         ScrollView {
             MapView(coordinate: landmark.locationCoordinate)
@@ -18,8 +24,11 @@ struct LandmarkDetail: View {
                 .offset(y:-130)
                 .padding(.bottom, -130)
             VStack(alignment: .leading){
-                Text(landmark.name)
-                    .font(.title)
+                HStack{
+                    Text(landmark.name)
+                        .font(.title)
+                    FavoriteButtonView(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                }
                 HStack{
                     Text(landmark.park)
                         .font(.subheadline)
@@ -39,10 +48,11 @@ struct LandmarkDetail: View {
         .navigationTitle(landmark.name)
         .navigationBarTitleDisplayMode(.inline)
     }
+    
 }
 
 struct LandmarkDetail_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkDetail(landmark: landmarks[0])
+        LandmarkDetail(landmark: ModelData().landmarks[0])
     }
 }

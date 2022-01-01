c418b968b2019c05e81ef9dd7fab4a6e258976b6
Deforeturn <deforeturn@gmail.com>
2022-01-01 15:52:54 +0900

    SiwftUITutorial - Ongoing Drawing and Animation
    Done Creating and Combining Views.

---
+++ App/AppleDev/SwiftUITutorials/LandMark/LandMark/Views/New Group/RotatedBadgeSymbol.swift
@@ -0,0 +1,25 @@
+//
+//  RotatedBadgeSymbol.swift
+//  LandMark
+//
+//  Created by Deforeturn on 1/1/22.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
